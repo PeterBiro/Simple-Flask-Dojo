9f42e2a06289e4e875caa5fca216763e0541ada3
hankoattila <hattilla4@gmail.com>
2017-05-11 09:47:15 +0200

    main.py 1

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
+@app.route("/request-counter")
+def request_counter():
+    return render_template("counter.html")
+
+
+@app.route("/request-counter", methods=['POST'])
+def request_counter_post():
+    return render_template("counter.html")
+
+
+@app.route("/")
+def root():
+    return render_template("first.html")
+
+
 def main():
-    pass
+    app.run(debug=True)
 
 if __name__ == '__main__':
-    main() 
+    main()
